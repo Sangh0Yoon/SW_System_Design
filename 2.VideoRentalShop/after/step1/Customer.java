d1e88a8b2c6666aebba20f44ad6417b2fb5a3321
ehfhfjd2@naver.com <ehfhfjd2@naver.com>
2023-03-09 13:54:22 +0900

    School assignment
    School assignment

---
+++ 2.VideoRentalShop/after/step1/Customer.java
@@ -0,0 +1,70 @@
+package VideoRentalShop.step1;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+class Customer {
+	private String _name;
+	private Vector<Rental> _rentals = new Vector();
+
+	public Customer(String name) {
+		_name = name;
+	};
+
+	public void addRental(Rental arg) {
+		_rentals.addElement(arg);
+	}
+
+	public String getName() {
+		return _name;
+	};
+
+	public String statement() {
+		String result = "Rental Record for " + getName() + "\n";
+		for (Rental each: _rentals) {
+			// show figures for this rental
+			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+		}
+
+		// add footer lines
+		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+		result += "You earned " + String.valueOf(getTotalFrequentRentersPoints()) + " frequent renter points";
+
+		return result;
+	}
+	public String htmlStatement() {
+		String result = "<H1>Rental Record for <EM>" + getName() + "</EM></H1><P>\n";
+		for (Rental each: _rentals) {
+			// show figures for this rental
+			result += "\t" + each.getMovie().getTitle() + ": " + String.valueOf(each.getCharge()) + "<BR>\n";
+		}
+		// add footer lines
+		result += "<P>You owe <EM> " + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+		result += "You earned <EM>" + String.valueOf(getTotalFrequentRentersPoints()) + "</EM> frequent renter points<P>";
+		
+		return result;
+	}
+	public double getTotalCharge() {
+		double result = 0;
+		for(Rental each: _rentals) result += each.getCharge();
+		return result;
+	}
+	public double getTotalFrequentRentersPoints() {
+		int result = 0;
+		for(Rental each: _rentals) result += each.getFrequentRenterPoints();
+		return result;
+	}
+//	public static void main(String[] args) {
+//		Customer customer1 = new Customer("David");
+//		Movie movie1 = new Movie("Madagascar", Movie.CHILDRENS);
+//		Rental rental1 = new Rental(movie1, 6); // 6 day rental
+//		Movie movie2 = new Movie("Star Wars", Movie.NEW_RELEASE);
+//		Rental rental2 = new Rental(movie2, 2); // 2 day rental
+//		Movie movie3 = new Movie("Gone with the Wind", Movie.REGULAR);
+//		Rental rental3 = new Rental(movie3, 8); // 8 day rental
+//		customer1.addRental(rental1);
+//		customer1.addRental(rental2);
+//		customer1.addRental(rental3);
+//		System.out.println(customer1.statement()) ;
+//	}
+}
