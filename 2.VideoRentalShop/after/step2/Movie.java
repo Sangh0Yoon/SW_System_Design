8204ac2600b6af25144c84a77ecf66abf5ca92ae
SanghoYoon <100823955+Sangh0Yoon@users.noreply.github.com>
2023-03-15 13:10:20 +0900

    Update Movie.java

--- 2.VideoRentalShop/after/step2/Movie.java
+++ 2.VideoRentalShop/after/step2/Movie.java
@@ -6,35 +6,33 @@ public class Movie {
 	public static final int CHILDRENS = 2;
 	private String _title;
 	private Price _price;
-	private int _priceCode;
 
 	public Movie(String title, int priceCode) {
 		_title = title;
 		setPriceCode(priceCode);
 	}
-
 	public int getPriceCode() {
-		return _priceCode;
+		return _price.getPriceCode();
 	}
-
 	public void setPriceCode(int arg) {
-		switch(arg) {
-		case REGULAR: _price = new RegularPrice(); break;
-		case CHILDRENS: _price = new ChildrenPrice(); break;
-		case NEW_RELEASE: _price = new NewReleasePrice(); break;
-		default: throw new IllegalArgumentException("Incorrect Price Code");
+		switch (arg) {
+        case Movie.REGULAR:
+        	_price = new RegularPrice(); break;
+        case Movie.NEW_RELEASE:
+        	_price = new NewReleasePrice(); break;
+        case Movie.CHILDRENS:
+        	_price = new ChildrenPrice(); break;
+        default:
+            throw new IllegalArgumentException("Incorrect Price Code");
 		}
 	}
-
 	public String getTitle() {
 		return _title;
 	}
-
 	public double getCharge(int daysRented) {
 		return _price.getCharge(daysRented);
 	}
-
-	int getFrequentRenterPoints(int daysRented) {
+	public int getFrequentRenterPoints(int daysRented) {
 		return _price.getFrequentRenterPoints(daysRented);
 	};
 }
