76916ff9fe4304bac9fbcc92f6a48c933bc4457c
SanghoYoon <100823955+Sangh0Yoon@users.noreply.github.com>
2023-03-15 13:00:53 +0900

    Update Customer.java

--- 2.VideoRentalShop/after/step1/Customer.java
+++ 2.VideoRentalShop/after/step1/Customer.java
@@ -1,11 +1,10 @@
 package VideoRentalShop.step1;
 
-import java.util.Enumeration;
 import java.util.Vector;
 
 class Customer {
 	private String _name;
-	private Vector<Rental> _rentals = new Vector();
+	private Vector<Rental> _rentals = new Vector<Rental>();
 
 	public Customer(String name) {
 		_name = name;
@@ -21,12 +20,12 @@ class Customer {
 
 	public String statement() {
 		String result = "Rental Record for " + getName() + "\n";
+		
 		for (Rental each: _rentals) {
-			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+			result += "\t" + each.getMovie().getTitle() + "\t" + 
+						String.valueOf(each.getCharge()) + "\n";
 		}
 
-		// add footer lines
 		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
 		result += "You earned " + String.valueOf(getTotalFrequentRentersPoints()) + " frequent renter points";
 
@@ -34,37 +33,41 @@ class Customer {
 	}
 	public String htmlStatement() {
 		String result = "<H1>Rental Record for <EM>" + getName() + "</EM></H1><P>\n";
+		
 		for (Rental each: _rentals) {
-			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + ": " + String.valueOf(each.getCharge()) + "<BR>\n";
 		}
-		// add footer lines
+		
 		result += "<P>You owe <EM> " + String.valueOf(getTotalCharge()) + "</EM><P>\n";
 		result += "You earned <EM>" + String.valueOf(getTotalFrequentRentersPoints()) + "</EM> frequent renter points<P>";
 		
 		return result;
 	}
 	public double getTotalCharge() {
-		double result = 0;
-		for(Rental each: _rentals) result += each.getCharge();
-		return result;
+		double charge = 0;
+		
+		for(Rental each: _rentals) charge += each.getCharge();
+		
+		return charge;
 	}
 	public double getTotalFrequentRentersPoints() {
-		int result = 0;
-		for(Rental each: _rentals) result += each.getFrequentRenterPoints();
-		return result;
+		int points = 0;
+		
+		for(Rental each: _rentals) points += each.getFrequentRenterPoints();
+		
+		return points;
 	}
-//	public static void main(String[] args) {
-//		Customer customer1 = new Customer("David");
-//		Movie movie1 = new Movie("Madagascar", Movie.CHILDRENS);
-//		Rental rental1 = new Rental(movie1, 6); // 6 day rental
-//		Movie movie2 = new Movie("Star Wars", Movie.NEW_RELEASE);
-//		Rental rental2 = new Rental(movie2, 2); // 2 day rental
-//		Movie movie3 = new Movie("Gone with the Wind", Movie.REGULAR);
-//		Rental rental3 = new Rental(movie3, 8); // 8 day rental
-//		customer1.addRental(rental1);
-//		customer1.addRental(rental2);
-//		customer1.addRental(rental3);
-//		System.out.println(customer1.statement()) ;
-//	}
+	public static void main(String[] args) {
+		Customer customer1 = new Customer("David");
+		Movie movie1 = new Movie("Madagascar", Movie.CHILDRENS);
+		Rental rental1 = new Rental(movie1, 6); // 6 day rental
+		Movie movie2 = new Movie("Star Wars", Movie.NEW_RELEASE);
+		Rental rental2 = new Rental(movie2, 2); // 2 day rental
+		Movie movie3 = new Movie("Gone with the Wind", Movie.REGULAR);
+		Rental rental3 = new Rental(movie3, 8); // 8 day rental
+		customer1.addRental(rental1);
+		customer1.addRental(rental2);
+		customer1.addRental(rental3);
+		System.out.println(customer1.statement()) ;
+	}
 }
