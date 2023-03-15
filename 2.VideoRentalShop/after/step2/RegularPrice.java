0cd43d4f2e08461206e2f7c090aeb97cc3b6b3d4
SanghoYoon <100823955+Sangh0Yoon@users.noreply.github.com>
2023-03-15 13:10:58 +0900

    Update RegularPrice.java

--- 2.VideoRentalShop/after/step2/RegularPrice.java
+++ 2.VideoRentalShop/after/step2/RegularPrice.java
@@ -3,8 +3,8 @@ package VideoRentalShop.step2;
 public class RegularPrice extends Price{
 	public int getPriceCode() {return Movie.REGULAR;}
 	public double getCharge(int daysRented) {
-		double result = 2;
-		if(daysRented > 2) result += (daysRented - 2) * 1.5;
-		return result;
+		double charge = 2;
+		if(daysRented > 2) charge += (daysRented - 2) * 1.5;
+		return charge;
 	}
 }
