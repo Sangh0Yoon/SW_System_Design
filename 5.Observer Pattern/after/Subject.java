06639896a1e042d27e27dc17378178c80c42629e
ehfhfjd2@naver.com <ehfhfjd2@naver.com>
2023-04-06 13:49:43 +0900

    Observer Pattern
    Observer Pattern

---
+++ 5.Observer Pattern/after/Subject.java
@@ -0,0 +1,20 @@
+package ObserverPattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subject {
+	private List<Observer> observers = new ArrayList<>();
+	
+	public void attach(Observer observer) {
+		observers.add(observer);
+	}
+	public void detach(Observer observer) {
+		observers.remove(observer);
+	}
+	protected void notify_() {
+		for(Observer observer : observers) {
+			observer.update();
+		}
+	}
+}
